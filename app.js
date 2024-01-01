6d96b77a400a851038a02e2403a52deb43633613
mehran feriduni <m3hr4nus@gmail.com>
2024-01-01 15:44:40 +0330

    run project typescript

---
+++ app.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(1, 4));
+console.log('1', '4');
---
+++ app.ts
@@ -0,0 +1,7 @@
+
+
+function sum(a:number,b:number){
+  return a+b;
+}
+console.log(sum(1,4));
+console.log('1','4');
