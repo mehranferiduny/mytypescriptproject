17f2711a1b428d6cf917b3fb5ac9884da3535959
mehran feriduni <m3hr4nus@gmail.com>
2024-01-02 07:14:15 +0330

    tsc option --watch

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ function sum(a, b) {
     return a + b;
 }
 console.log(sum(1, 4));
-console.log('1', '4');
+console.log(sum(7, 8));
+// console.log(sum('1','4'));
--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 
-
 function sum(a:number,b:number){
   return a+b;
 }
 console.log(sum(1,4));
-console.log('1','4');
+console.log(sum(7,8));
+// console.log(sum('1','4'));
