0ab5af1257804951ef383bb1277f99a34af2cf67
mehran feriduni <m3hr4nus@gmail.com>
2024-01-04 00:16:17 +0330

    add type nan array any infnity

--- app.js
+++ app.js
@@ -14,3 +14,26 @@ var z = 'hello';
 r = b + b;
 console.log(r);
 console.log(typeof (a));
+//!NaN
+console.log(4 - 'hello');
+//!infinity
+console.log(0 / 2);
+console.log(-0 / 2);
+//!Any
+var a1;
+a1 = 2;
+a1 = 'hello';
+a1 = false;
+//!Arrays
+var arr1 = [1, "hii", false];
+var arr2 = [1, 4, "hii"];
+var arr3 = [1, 2, 'hii'];
+var arr4 = [1, 'hii'];
+arr1.push('hello');
+arr1.push(2);
+arr2.push('sss');
+console.log(arr1[1]);
+console.log(arr1);
+//!array in array
+var images = [[225, 250, 240], [325, 346, 125]];
+console.log(images);
--- app.ts
+++ app.ts
@@ -18,3 +18,35 @@ r=b+b;
 console.log(r);
 
 console.log(typeof(a));
+
+//!NaN
+console.log(4 - 'hello');
+
+//!infinity
+console.log(0/2);
+console.log(-0/2);
+
+
+//!Any
+let a1: any;
+a1=2;
+a1='hello'
+a1 = false;
+
+//!Arrays
+let arr1=[1,"hii",false];
+let arr2: number[]=[1,4,"hii"];
+let arr3: Array<any>=[1,2,'hii'];
+let arr4: Array<string>=[1,'hii'];
+arr1.push('hello');
+arr1.push(2);
+arr2.push('sss');
+console.log(arr1[1]);
+console.log(arr1);
+//!array in array
+let images:number[][]=[[225,250,240],[325,346,125]]
+console.log(images);
+
+
+
+
