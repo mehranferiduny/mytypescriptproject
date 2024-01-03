e1bbdc3e8468081fe943665ab2c64f67dd950a17
mehran feriduni <m3hr4nus@gmail.com>
2024-01-03 11:42:57 +0330

    type 1 in typescript

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ function sum(a, b) {
 console.log(sum(1, 4));
 console.log(sum(7, 8));
 // console.log(sum('1','4'));
+var a;
+var b = 10;
+var c = 'hello';
+var d;
+var g;
+var r;
+var z = 'hello';
+r = b + b;
+console.log(r);
+console.log(typeof (a));
--- app.ts
+++ app.ts
@@ -5,3 +5,16 @@ function sum(a:number,b:number){
 console.log(sum(1,4));
 console.log(sum(7,8));
 // console.log(sum('1','4'));
+
+let a;
+let b=10;
+let c='hello';
+let d:string;
+let g:boolean;
+let r:number;
+let z:string = 'hello';
+
+r=b+b;
+console.log(r);
+
+console.log(typeof(a));
